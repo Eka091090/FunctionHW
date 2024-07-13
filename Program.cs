a42f2ee3bfc07069b864ed76ebc4d48fa0021e5f
Eka091090 <grinik1990@icloud.com>
2024-07-13 17:05:15 +0700

    end home work

--- Program.cs
+++ Program.cs
@@ -1,69 +1,135 @@
-﻿void Main()
-{
-    string value = userInput();
-    System.Console.WriteLine(value);
-    ifStop(value);
-}
+﻿// EX1 
 
-
-string userInput()
-{
-    while(true)
-    {
-        System.Console.Write($"Введите целое число или 'q' для выхода: ");
-        string? number = Console.ReadLine();
-        // if(number == "q")
-        //     break;
-        // System.Console.WriteLine(number);
-        // return number;
-    }
-}
-
-void ifStop(string value)
-{
-    if(value == "q")
-        System.Console.WriteLine("[STOP]");
-        userInput.break;
-
-
-}
-
-
-
-Main();
-
-// using System;
-// class Program
+// void Main()
 // {
-//     static void Main()
-//     {
-//         while (true) // Бесконечный цикл
-//         {
-//             Console.Write("Введите число или 'q' для выхода: ");
-//             string input = Console.ReadLine(); // Чтение строки ввода пользователя
-//             // if (input == "q") // Проверка на ввод 'q' для выхода
-//             // {
-//             //     Console.WriteLine("[STOP]");
-//             //     break;
-//             // }
-//             // int number;
-//             // int sum = 0;
-//             // if (int.TryParse(input, out number)) // Проверка, является ли ввод числом{
-//             //     sum = 0;
-//             // while (number > 0) // Вычисление суммы цифр числа
-//             // {
-//             //     sum += number % 10; // Добавление последней цифры к сумме
-//             //     number /= 10; // Удаление последней цифры из числа
-//             // }
-//             // if (sum % 2 == 0) // Проверка, является ли сумма цифр четной
-//             // {
-//             //     Console.WriteLine("[STOP]");
-//             //     break;
-//             // }
-//         }
-//         // else // Если ввод не является числом и не 'q', повторить запрос
-//         // {
-//         //     Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое числоили 'q'.");
-//         // }
-//     }
+//     string value = userInput();
 // }
+
+// string userInput()
+// {
+//     while(true)
+//     {
+//         System.Console.Write($"Введите целое число или 'q' для выхода: ");
+//         string? number = Console.ReadLine();
+//         if(number == "q")
+//         {
+//             Console.WriteLine("[STOP]");
+//             break;
+//         }
+//         int num;
+//         int sum = 0;
+//         if (int.TryParse(number, out num)) 
+//             sum = 0;
+//         while (num > 0) 
+//         {
+//             sum += num % 10; 
+//             num /= 10; 
+//             System.Console.WriteLine(sum);;
+//         }
+//         if (sum % 2 == 0) 
+//         {
+//             Console.WriteLine("[STOP]");
+//             break;
+//         }
+//     }
+//     return "";
+// }
+
+// Main();
+
+
+
+// EX2
+
+// using System.Data;
+
+// void Main()
+// {
+//     int[] array = GenerateArray();
+//     PrintArray(array);
+//     CountNymbers(array);
+//     Console.WriteLine("Количество четныхчисел: " + CountNymbers(array));
+// }
+
+// int CountNymbers(int[] array)
+// {
+//     int count = 0;
+//     for(int i = 0; i < array.Length; i++)
+//         if(array[i] % 2 == 0)
+//             count++;
+//     return count;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//         System.Console.Write(array[i] + " ");
+//     System.Console.WriteLine();
+// }
+
+
+// int[] GenerateArray()
+// {
+//     Random arraySize = new Random();
+//     int size = arraySize.Next(1, 20);
+
+//     int[] tempArray = new int[size];
+
+//     Random rand = new Random();
+
+//     for(int i = 0; i < tempArray.Length; i++)
+//         tempArray[i] = rand.Next(100, 1000);
+    
+//     return tempArray;
+// }
+
+// Main();
+
+
+// EX3
+
+// void Main()
+// {
+//     int[] array = GenerateArray();
+//     PrintArray(array);
+//     PrintArray(reversArray(array));
+
+// }
+
+// int[] reversArray(int[] array)
+// {
+//     int[] temp = new int[array.Length];
+//     for(int i = 0; i < array.Length; i++)
+
+//         {
+//         temp[i] += array[array.Length - 1 - i];
+//         }
+//     return temp;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//         System.Console.Write(array[i] + " ");
+//     System.Console.WriteLine();
+// }
+
+
+// int[] GenerateArray()
+// {
+//     Random arraySize = new Random();
+//     int size = arraySize.Next(1, 20);
+
+//     int[] tempArray = new int[size];
+
+//     Random rand = new Random();
+
+//     for(int i = 0; i < tempArray.Length; i++)
+//         tempArray[i] = rand.Next(100, 1000);
+    
+//     return tempArray;
+// }
+
+// Main();
